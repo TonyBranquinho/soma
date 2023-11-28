5e996ca81fa001eff76f517f64a14729a5473eee
Tony <antony.branquinho@gmail.com>
2023-11-28 13:20:31 -0300

    Bem facil

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soma {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int x, y, soma;
+
+            Console.Write("Digite o valor de X: ");
+            x = int.Parse(Console.ReadLine(), CI);
+            Console.Write("Digite o valor de Y: ");
+            y = int.Parse(Console.ReadLine(), CI);
+
+            soma = x + y;
+
+            Console.WriteLine("Soma = " + soma, CI);
+        }
+    }
+}
